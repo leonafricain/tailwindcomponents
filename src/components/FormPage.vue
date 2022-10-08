73131a02653da46208340841aa2c112470765ccd
leonafricain <kimleonafricain@gmail.com>
2022-10-09 00:52:33 +0100

    2 commit

---
+++ src/components/FormPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+        <!-- login section -->
+        <div>Login section</div> 
+
+        <!-- sign up section -->
+        <div>Sign au section</div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
